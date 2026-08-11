e7ad871aeba3217a23895999abeaa848618c8863
Abinaya Thirunavukarasu <abinayathirunavukarasu308@gmail.com>
2026-08-12 05:02:28 +0530

    Add unit test for JobListingReqDto constructors

--- src/test/java/com/careercrafter/CareercrafterApplicationTests.java
+++ src/test/java/com/careercrafter/CareercrafterApplicationTests.java
@@ -1,5 +1,6 @@
 package com.careercrafter;
 
+import com.careercrafter.dto.request.JobListingReqDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,14 @@ class CareercrafterApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    void testJobListingReqDtoConstructors() {
+        var dto1 = new JobListingReqDto("Developer", "Job Description");
+        var dto2 = new JobListingReqDto("Developer", "Job Description", 1L);
+
+        org.junit.jupiter.api.Assertions.assertNotNull(dto1);
+        org.junit.jupiter.api.Assertions.assertNotNull(dto2);
     }
 
 }
